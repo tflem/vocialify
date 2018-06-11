0aa377522720390254cc049898f04b19f8a1fc3f
Tim Fleming <tfleming26@hotmail.com>
2018-06-11 11:19:05 -0600

    Add plug dependency

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Vocialify.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:plug, "1.5.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"}
     ]
